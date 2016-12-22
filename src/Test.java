0b409e30b72248c1a6d914a9482f71d7b40ac015
zukihxy <14302010050@fudan.edu.cn>
2016-12-22 12:58:46 +0800

    1.1 url

---
+++ src/Test.java
@@ -0,0 +1,8 @@
+
+public class Test {
+	public static void main(String[] args){
+		URLParser u = new URLParser();
+		u.parsingURL("https://www.baidu.com/s/pub/ietf/uri?wd=dfaew&rsv_spt=1&rsv_iqid=0xf2 #Related");
+		
+	}
+}
--- src/URLParser.java
+++ src/URLParser.java
@@ -1,6 +1,45 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class URLParser {
+	private String schema;
+	private String authority;
+	private String path;
+	private String query;
+	private String fragment;
+	
+	public String getSchema() {
+		return schema;
+	}
+
+	public String getAuthority() {
+		return authority;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public String getQuery() {
+		return query;
+	}
+
+	public String getFragment() {
+		return fragment;
+	}
+
 	public void parsingURL(String url) {
-		
+		String reg = "^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\\?([^#]*))?(#(.*))?";
+		Pattern pattern = Pattern.compile(reg);
+		Matcher matcher = pattern.matcher(url);
+		if (matcher.matches()) {
+			schema = matcher.group(2);
+			authority = matcher.group(4);
+			path = matcher.group(5);
+			query = matcher.group(7);
+			fragment = matcher.group(9);
+		} else {
+			System.err.println("Can not match the URL!");
+		}
 	}
 }
